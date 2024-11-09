dd1edc93dfbba7ea25b10bc154cb6ec7bf8e2010
AlphaTanmoy <dtanmoy169@gmail.com>
2024-11-10 00:31:29 +0530

    update

--- src/main/kotlin/com/AlphaStore/APIGateway/ApiGatewayApplication.kt
+++ src/main/kotlin/com/AlphaStore/APIGateway/ApiGatewayApplication.kt
@@ -1,13 +1,11 @@
-package com.AlphaStore.APIGateway
+package com.alphaStore.APIGateway
 
-import org.springframework.boot.SpringApplication
 import org.springframework.boot.autoconfigure.SpringBootApplication
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient
+import org.springframework.boot.runApplication
 
 @SpringBootApplication
-@EnableDiscoveryClient
 class ApiGatewayApplication
 
 fun main(args: Array<String>) {
-    SpringApplication.run(ApiGatewayApplication::class.java, *args)
+	runApplication<ApiGatewayApplication>(*args)
 }
