8f3c25550a4c2dc8798eb92ce800d02d3dcc4a72
AlphaTanmoy <dtanmoy169@gmail.com>
2024-11-09 23:51:41 +0530

    basefile added

---
+++ src/main/kotlin/com/AlphaStore/APIGateway/ApiGatewayApplication.kt
@@ -0,0 +1,13 @@
+package com.AlphaStore.APIGateway
+
+import org.springframework.boot.SpringApplication
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient
+
+@SpringBootApplication
+@EnableDiscoveryClient
+class ApiGatewayApplication
+
+fun main(args: Array<String>) {
+    SpringApplication.run(ApiGatewayApplication::class.java, *args)
+}
